c9e069f266385fba3582727a081f667a559bfb61
unk pn <zver2006.11@gmail.com>
2025-02-19 17:25:57 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,46 @@
+const filterItems = document.querySelectorAll('.cars-filter li');
+const carItems = document.querySelectorAll('.car');
+const carsContent = document.getElementById('cars-content');
 
+const carField = document.getElementById('car')
+const nameField = document.getElementById('name')
+const phoneField = document.getElementById('phone')
+
+filterItems.forEach(item => {
+    item.onclick = () => {
+        filterItems.forEach(el => el.classList.remove('active'));
+        item.classList.add('active');
+
+        const filterText = item.textContent.toLowerCase();
+
+        carItems.forEach(car => {
+            if (filterText === 'все марки' || car.querySelector('h4').textContent.toLowerCase().includes(filterText)) {
+                car.style.display = 'flex';
+            } else {
+                car.style.display = 'none';
+            }
+        });
+
+        carsContent.scrollIntoView({behavior:'instant'});
+    };
+});
+
+document.getElementById('order-action').addEventListener('click', function() {
+    const fields = [carField, nameField, phoneField];
+
+    let hasError = false;
+
+    fields.forEach((field) => {
+        if (field.value.trim() === '') {
+            field.style.borderColor ='red';
+            hasError = true;
+        } else {
+            field.style.borderColor = 'white';
+        }
+    });
+
+    if (!hasError) {
+        alert('Спасибо за заявку! Мы скоро свяжемся с вами');
+        fields.forEach(field => field.value = '');
+    }
+});
